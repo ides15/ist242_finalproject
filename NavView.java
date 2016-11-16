2bed021423f27b6b065793f892f16c1f157ed56a
nkc5101 <nkc5101@psu.edu>
2016-11-16 15:15:44 -0500

    Add files via upload

---
+++ NavView.java
@@ -0,0 +1,17 @@
+package finalproject;
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nkc5101
+ */
+public class NavView extends JFrame{
+    
+}

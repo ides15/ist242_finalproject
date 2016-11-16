94af45ac3e90a6f330ec46e80c4289c5d086a566
nkc5101 <nkc5101@202-WD14.up.ist.local>
2016-11-16 15:35:32 -0500

    Nate's Revision

---
+++ finalproject/FinalProject/src/finalproject/ButtonsPanel.java
@@ -0,0 +1,49 @@
+package finalproject;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nkc5101
+ */
+public class ButtonsPanel extends JPanel{
+    
+  
+    
+   private JButton optionsButton;
+   private JButton instructionsButton;
+   private JButton creditsButton;
+   private JButton homeButton;
+   private JButton profileButton;
+   private JButton payrollButton;
+   private JButton logoffButton;
+    
+    ButtonsPanel(){
+        optionsButton = new JButton("Options");
+        instructionsButton = new JButton("Instructions");
+        creditsButton = new JButton("Credits");
+        homeButton = new JButton("Home");
+        profileButton = new JButton("Profile");
+        payrollButton = new JButton("Payroll");
+        logoffButton = new JButton("Logoff");
+        
+        add(homeButton);
+        add(profileButton);
+        add(payrollButton);
+        add(instructionsButton);
+        add(optionsButton);
+        add(instructionsButton);
+        add(logoffButton);
+    }
+
+    
+}

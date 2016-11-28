559055c605328d00a36a63c60893173b487b3b7e
jci5048 <jci5048@202-WD23.up.ist.local>
2016-11-28 14:59:27 -0500

    splash screen

--- finalproject/FinalProject/src/finalproject/View.java
+++ finalproject/FinalProject/src/finalproject/View.java
@@ -6,10 +6,7 @@
 package finalproject;
 
 import java.awt.BorderLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 
 /**
  *
@@ -21,35 +18,6 @@ public class View extends JFrame{
     private NavPanel nav;
     private SplashPanel splash;
     
-    // BUTTONS  
-    private JButton homeButton;
-    private JButton profileButton;
-    private JButton payrollButton;
-    private JButton instructionsButton;
-    private JButton creditsButton;
-    private JButton logoffButton;
-    private JButton createUserButton;
-    private JButton submitUserButton;
-    private JButton loginButton;
-    private JButton saveProfileButton;
-    
-    // LABELS
-    private JLabel welcomeLabel;
-    private JLabel signInLabel;
-    private JLabel createUserLabel;
-    private JLabel instructionsLabel;
-    private JLabel payrollLabel;
-    private JLabel profileLabel;
-    private JLabel creditLabel;
-    
-    // TEXT FIELDS
-    private JTextField username;
-    private JTextField password;
-    private JTextField email;
-    private JTextField age;
-    private JTextField address;
-    private JTextField name;
-    
     public View(Model model){
         
         super();
@@ -60,38 +28,7 @@ public class View extends JFrame{
         
         add(nav, BorderLayout.SOUTH);
         add(splash, BorderLayout.CENTER);
-    }
-    
-    public JButton getProfile(){
-        return profileButton;
-    }
-    
-    public JButton getLogin(){
-        return loginButton;
-    }
-    
-    public JButton getCreateUser(){
-        return createUserButton;
-    }
-    
-    public JButton getPayroll(){
-        return payrollButton;
-    }
-    
-    public JButton getHome(){
-        return homeButton;
-    }
-    
-    public JButton getInstrucitons(){
-        return instructionsButton;
-    }
-    
-    public JButton getCredit(){
-        return creditsButton;
-    }
-
-    public JButton getLogoff(){
-        return logoffButton;
+        
     }
     
 }

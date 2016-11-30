1be8bf0c4bc33e83dd607d32d15bebd2ffc445f6
nkc5101 <nkc5101@202-WD14.up.ist.local>
2016-11-30 15:04:42 -0500

    added payroll panel

---
+++ finalproject/FinalProject/src/finalproject/PayrollPanel.java
@@ -0,0 +1,46 @@
+package finalproject;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nkc5101
+ */
+public class PayrollPanel extends JPanel{
+    private JLabel company;
+    private JLabel currentPeriod;
+    private JLabel YTDPay;
+    private JLabel taxes;
+    private JLabel salary;
+    private JLabel hoursWorked;
+
+    public PayrollPanel(Person person) {
+        company = new JLabel();
+        company.setText("Company :" + person.getCompany());
+        currentPeriod = new JLabel("Current Paycheck:" + person.getPaycheck());
+        YTDPay = new JLabel("YTD Pay: " + person.getYTDPay());
+        taxes = new JLabel("Taxes: " + person.getTaxes());
+        salary = new JLabel("Salary: " + person.getSalary());
+        hoursWorked = new JLabel("Hours Worked: " + person.getHours());
+        
+        add(company);
+        add(currentPeriod);
+        add(YTDPay);
+        add(taxes);
+        add(salary);
+        add(hoursWorked);
+        
+    }
+   
+    
+    
+    
+}
